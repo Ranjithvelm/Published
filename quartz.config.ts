de2917a06a5972146a40b4ba5f859e4e33451de7
Ranjithvel <ranjithvelmohanasundaram@gmail.com>
2024-08-17 21:14:05 +0530

    Quartz sync: Aug 17, 2024, 9:14 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 GATE preparation",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://ranjithvelm.github.io/Published/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
